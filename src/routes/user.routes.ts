3f0415b145f4190cb71638f8d69b6d52aa786118
sujeet kumar <ksujeetkumar7678@gmail.con>
2024-01-08 19:11:22 +0530

    Add new routes and controllers for user profile and history

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import {
   getAccessToken,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logout,
   registerUser,
   updateCoverImg,
   updatePassword,
   updateProfileImg,
+  updateUserDetails,
 } from "../controllers/user.controllers.js";
 import { isAuthorized } from "../middlewares/auth.middleware.js";
 
@@ -23,16 +26,25 @@ userRouter.route("/register").post(
 );
 // login
 userRouter.route("/login").post(loginUser);
-userRouter.route("/get-token").get(getAccessToken)
+userRouter.route("/get-token").get(getAccessToken);
 
 //protected route
 userRouter.route("/logout").post(isAuthorized, logout);
+
 userRouter.route("/update-password").patch(isAuthorized, updatePassword);
+
 userRouter
   .route("/update-avatar")
   .patch(isAuthorized, upload.single("avatar"), updateProfileImg);
+
 userRouter
   .route("/update-cover-img")
   .patch(isAuthorized, upload.single("coverImg"), updateCoverImg);
+
+userRouter.route("/update-user").patch(isAuthorized, updateUserDetails);
+
+userRouter.route("/c/:username").get(isAuthorized, getUserChannelProfile);
+
+userRouter.route("/history").get(isAuthorized, getWatchHistory);
 
 export default userRouter;
