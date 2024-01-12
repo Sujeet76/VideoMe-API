d4bfb21ce86496f350760980c153c4dd151dc290
sujeet kumar <ksujeetkumar7678@gmail.con>
2024-01-12 19:48:22 +0530

    Add health check route and controller

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,9 @@ import videoRouter from "./routes/video.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import healthcheckRouter from "./routes/healthCheck.routes.js";
 
+app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/videos", videoRouter);
---
+++ src/controllers/healthCheck.controllers.ts
@@ -0,0 +1,10 @@
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/AsyncHandler.js";
+
+const healthcheck = asyncHandler(async (req, res) => {
+  //TODO: build a healthcheck response that simply returns the OK status as json with a message
+  res.status(200).json(new ApiResponse(200, "OK", "OK"));
+});
+
+export { healthcheck };
